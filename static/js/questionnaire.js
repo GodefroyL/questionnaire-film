7b212cbc7b0f1b5f714ba41c9332bcb6d63ce9e6
GodefroyL <godefroy.lecluse@gmail.com>
2026-03-18 02:18:06 +0200

    debbug resultat v9

--- static/js/questionnaire.js
+++ static/js/questionnaire.js
@@ -25,14 +25,14 @@ function affichageDonnee() {
       container.innerHTML = `
         <h2>${item.categorie}</h2>
         <p>${item.intitule}</p>
-        <input type="text" id="reponse">`;
+        <input type="text" name="reponse">`;
     }
     if (!item.programme) {
       container.innerHTML += `<p><em>Cette question porte sur un film hors programme.</em></p>`;
     }
     index++;
   } else {
-    document.getElementById("detailResultats").style.display = "block";
+    document.getElementsByName("detailResultats").style.display = "block";
     document.getElementById("valider").style.display = "none"
     container.innerHTML = resultatsHTML();
   }
@@ -43,22 +43,24 @@ function valider() {
   if (index < donnee.length) {
     const item = donnee[index];
     // Sélectionner tous les inputs avec le nom "reponse"
-    const inputs = document.querySelectorAll('input[type="text"][id="reponse"]');
+    const inputs = document.querySelectorAll('input[type="text"][name="reponse"]');
 
     // Parcourir chaque input
+    reponse = ""
     inputs.forEach(input => {
         // Récupérer la valeur de l'input et supprimer les espaces en début et fin
-        let reponse = input.value.trim();
+        let valeur = input.value.trim();
 
         // Si la valeur n'est pas vide
-        if (reponse) {
+        if (valeur) {
             // Supprimer toute ponctuation et remplacer par un espace
-            reponse = reponse.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, ' ');
+            valeur = valeur.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, ' ');
             // Remplacer les espaces multiples par un seul espace
-            reponse = reponse.replace(/\s{2,}/g, ' ');
+            valeur = valeur.replace(/\s{2,}/g, ' ');
             // Convertir en minuscules
-            reponse = reponse.toLowerCase();
+            valeur = valeur.toLowerCase();
             // Ajouter chaque mot non vide à la liste
+            reponse += valeur
         }
     });
     console.log("reponse utilisateur : ", reponse)
@@ -84,7 +86,7 @@ function completezHTML(item) {
   `
   for (const element of item.intitule) {
     html += `${element} `;
-    html += `<input type="text" id="reponse"> `;
+    html += `<input type="text" name="reponse"> `;
   }
   html = html.slice(0,-33);
   html += `</p>`;
