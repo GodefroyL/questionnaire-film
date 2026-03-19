83c8f45bfbcb09f3d3fc9de0dbd399102dc3b853
GodefroyL <godefroy.lecluse@gmail.com>
2026-03-19 03:02:50 +0200

    ajout appli mobile

---
+++ application_mobile/app/src/main/java/com/example/dgl_film/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.dgl_film
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.webview)
+
+        // Configuration optimisée des paramètres
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            databaseEnabled = true
+            loadWithOverviewMode = true
+            useWideViewPort = true
+            cacheMode = WebSettings.LOAD_DEFAULT
+        }
+
+        // Configuration du WebViewClient pour intercepter les liens
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: android.webkit.WebResourceRequest?): Boolean {
+                return false
+            }
+        }
+
+        // Chargement de l'URL initiale
+        webView.loadUrl("https://godefroyl.github.io/questionnaire-film/")
+    }
+
+    override fun onBackPressed() {
+        val webView: WebView = findViewById(R.id.webview)
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            @Suppress("DEPRECATION")
+            super.onBackPressed()
+        }
+    }
+}
