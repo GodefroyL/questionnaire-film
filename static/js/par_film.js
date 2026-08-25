cb654ee814e358fe012cf4d4e043ddbbad23060b
GodefroyL <godefroy.lecluse@gmail.com>
2026-08-25 02:01:56 +0200

    corr par_film.js

--- static/js/par_film.js
+++ static/js/par_film.js
@@ -1,7 +1,6 @@
-fetch('../json/liste_film.json')
+fetch('json/liste_film.json')
     .then(reponse => reponse.json())
     .then(fichiersJSON => {
-        console.log("test")
         const conteneur = document.getElementById('accueil_film');
         let ligneActuelle = null;
         const listeCouleurs = ['var(--vert)', 'var(--jaune)', 'var(--rouge)', 'var(--bleu)', 'var(--marron)']
