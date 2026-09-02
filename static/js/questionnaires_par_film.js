1a7b981fa27a23387cfb4e537fd0780ef3d0804c
GodefroyL <godefroy.lecluse@gmail.com>
2026-09-03 00:47:35 +0200

    adaptation tel q par film

--- static/js/questionnaires_par_film.js
+++ static/js/questionnaires_par_film.js
@@ -2,11 +2,13 @@ fetch('../static/json/liste_film.json')
     .then(reponse => reponse.json())
     .then(fichiersJSON => {
         const conteneur = document.getElementById('accueil_film');
+        const isMobile = window.matchMedia('(max-width: 550px)').matches;
         let ligneActuelle = null;
         const listeCouleurs = ['var(--vert)', 'var(--jaune)', 'var(--rouge)', 'var(--bleu)', 'var(--marron)']
 
+        let film_par_colonne = isMobile ? 2 : 4;
         fichiersJSON.forEach((info_fichier, index) => {
-            if (index % 4 === 0) {
+            if (index % film_par_colonne === 0) {
                 ligneActuelle = document.createElement('div');
                 ligneActuelle.className = 'ligne';
                 conteneur.appendChild(ligneActuelle);
@@ -16,7 +18,7 @@ fetch('../static/json/liste_film.json')
             bouton.href = `https://godefroyl.github.io/questionnaire-film/html/questionnaire?film=${index}`;
             bouton.className = 'bouton_par_film';
             bouton.textContent = info_fichier.nom;
-            bouton.style.backgroundColor = listeCouleurs[(~~(index / 4)) % 5];
+            bouton.style.backgroundColor = listeCouleurs[(~~(index / film_par_colonne)) % 5];
 
             const image = document.createElement('img');
             image.src = `${info_fichier.image}`;
