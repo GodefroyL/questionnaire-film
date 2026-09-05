547e47f246cafb3bab502ce22f96450a8d59b7b3
GodefroyL <godefroy.lecluse@gmail.com>
2026-09-05 22:16:12 +0200

    corr resultats

--- static/js/resultat.js
+++ static/js/resultat.js
@@ -21,19 +21,21 @@ function recup_info () {
 
 function affichage_resultat() {
     let nombre_question_reussie = 0;
-    const nombre_question_totale = donneGlobales.resultats.length;
+    let nombre_question_totale = 0;
     console.log(nombre_question_totale);
     for(const id in donneGlobales.resultats) {
-        resultat = donneGlobales.resultats[id];
+        nombre_question_totale++;
+        const resultat = donneGlobales.resultats[id];
         console.log(resultat);
         const bouton = document.createElement('button');
         bouton.className = 'bouton_resultat';
+        bouton.id = `question${id}`
         bouton.textContent = id;
         if(resultat.reussi){
             bouton.style.backgroundColor = 'green';
             nombre_question_reussie++;
         } else {bouton.style.backgroundColor = 'red';}
-        bouton.addEventListener("click", affichage_detail(resultat));
+        bouton.addEventListener("click", () => affichage_detail(id, resultat));
         donneGlobales.resultat_par_question.appendChild(bouton)
     }
     const message_resultat = document.createElement('div');
@@ -42,20 +44,31 @@ function affichage_resultat() {
     donneGlobales.message_resultat.appendChild(message_resultat);
 }
 
-function affichage_detail(resultat) {
+function affichage_detail(id, resultat) {
+    donneGlobales.detail_resultat.innerHTML = '';
+// Affichage de la question
     const question = document.createElement('span');
     question.textContent = 'Question : ' + resultat.question;
     donneGlobales.detail_resultat.appendChild(question);
+
+// Affichage de la réponse de l'utilisateur
     const reponse_utilisateur = document.createElement('span');
     reponse_utilisateur.textContent = 'Votre réponse : ' + resultat.reponse;
-    if(resultat.reussi){reponse_utilisateur.style.color = 'green';} else {reponse_utilisateur.style.backgroundColor = 'red';}
+    if(resultat.reussi){reponse_utilisateur.style.color = 'green';}
+    else {
+        reponse_utilisateur.style.color = 'red';
+    // Affichage de la bonne réponse (uniquement si c'est faux)
+        const bonne_reponse = document.createElement('span');
+        bonne_reponse.textContent = 'Réponse attendue : ' + resultat.bonne_reponse;
+        bonne_reponse.style.color = 'green';
+        donneGlobales.detail_resultat.appendChild(bonne_reponse);}
     donneGlobales.detail_resultat.appendChild(reponse_utilisateur);
-    const bonne_reponse = document.createElement('span');
-    bonne_reponse.textContent = 'Réponse attendue : ' + resultat.bonne_reponse;
-    bonne_reponse.style.backgroundColor = 'green';
-    donneGlobales.detail_resultat.appendChild(bonne_reponse);
+    const bouton = document.getElementById(`question${id}`)
+    bouton.addEventListener('click', () => fermer_detail())
+
 }
+
+function fermer_detail() {donneGlobales.detail_resultat.innerHTML = ''}
 
 recup_info();
 affichage_resultat();
-console.log(document.body)
